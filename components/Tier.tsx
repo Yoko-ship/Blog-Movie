54828dd6707fbfc78f50c4681d5ca2b8f3e54c74
User <yuko.brandon13@gmail.com>
2025-04-26 11:32:44 +0500

    added tierlist

--- components/Tier.tsx
+++ components/Tier.tsx
@@ -1,83 +1,111 @@
-'use client'
-import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd"
-import { useState } from 'react'
-import data from "@/datas.json"
-import Image from "next/image"
-
-
-const initialData = {
-  S: [],
-  A: [],
-  B: [],
-  C: [],
-  D: [],
-  Values:data
-}
+"use client";
+import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
+import { useState } from "react";
+import data from "@/datas.json";
+import Image from "next/image";
+import classes from "@/app/tierlist/page.module.css";
+import { saveTierData } from "@/lib/helper";
+import tierData from "@/tierData.json"
+import { items } from "./structure";
+const initialData = tierData
 
 export default function TierList() {
-  const [tiers, setTiers] = useState(initialData)
+  const [tiers, setTiers] = useState(initialData);
 
   const onDragEnd = (result) => {
-    const { source, destination } = result
-    if (!destination) return
+    const { source, destination } = result;
+    if (!destination) return;
 
-    const sourceItems = Array.from(tiers[source.droppableId])
-    const [movedItem] = sourceItems.splice(source.index, 1)
+    if (source.droppableId === destination.droppableId) {
+      const items = Array.from(tiers[source.droppableId]);
+      const [movedItem] = items.splice(source.index, 1);
+      items.splice(destination.index, 0, movedItem);
+      setTiers((prev) => ({
+        ...prev,
+        [source.droppableId]: items,
+      }));
+    } else {
+      const sourceItems = Array.from(tiers[source.droppableId]);
+      const [movedItem] = sourceItems.splice(source.index, 1);
 
-    const destinationItems = Array.from(tiers[destination.droppableId])
-    destinationItems.splice(destination.index, 0, movedItem)
+      const destinationItems = Array.from(tiers[destination.droppableId]);
+      destinationItems.splice(destination.index, 0, movedItem);
 
-    setTiers({
-      ...tiers,
-      [source.droppableId]: sourceItems,
-      [destination.droppableId]: destinationItems
-    })
-  }
+      setTiers({
+        ...tiers,
+        [source.droppableId]: sourceItems,
+        [destination.droppableId]: destinationItems,
+      });
+    }
+  };
+
 
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      {Object.entries(tiers).map(([tier, items]) => (
-        <div key={tier} style={{ marginBottom: 20 }}>
-          <h2>{tier}-Tier</h2>
-          <Droppable droppableId={tier}>
-            {(provided) => (
-              <div
-                ref={provided.innerRef}
-                {...provided.droppableProps}
-                style={{
-                  minHeight: 100,
-                  background: '#f0f0f0',
-                  padding: 10,
-                  display: 'flex',
-                  gap: 10
-                }}
-              >
-                {items.map((item, index) => (
-                  <Draggable draggableId={item.id} index={index} key={item.id}>
-                    {(provided) => (
-                      <div
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                        style={{
-                          userSelect: 'none',
-                          padding: 10,
-                          background: 'white',
-                          borderRadius: 8,
-                          ...provided.draggableProps.style
-                        }}
+    <>
+      <div className={classes.saveDiv}>
+          <button onClick={() =>(saveTierData(tiers))}>Сохранить</button>
+        </div>      
+      <DragDropContext onDragEnd={onDragEnd}>
+        {Object.entries(tiers).map(([tier, items]) => (
+          <div key={tier} style={{ marginBottom: 20 }}>
+            <Droppable droppableId={tier}>
+              {(provided) => (
+                <div className={classes.tierName}>
+                  <h2
+                    className={
+                      tier === "S"
+                        ? classes.S
+                        : tier === "A"
+                        ? classes.A
+                        : tier === "B"
+                        ? classes.B
+                        : tier === "C"
+                        ? classes.C
+                        : tier === "D"
+                        ? classes.D
+                        : classes.imageTier
+                    }
+                  >
+                    {tier}
+                  </h2>
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.droppableProps}
+                    className={classes.droppableDiv}
+                  >
+                    {items.map((item, index) => (
+                      <Draggable
+                        draggableId={item.id}
+                        index={index}
+                        key={item.id}
                       >
-                        <Image src={`/images/${item.imageName}`} height={50} width={100} alt="" style={{ width: '100%', height: '100%', objectFit: 'cover' }}/>
-                      </div>
-                    )}
-                  </Draggable>
-                ))}
-                {provided.placeholder}
-              </div>
-            )}
-          </Droppable>
-        </div>
-      ))}
-    </DragDropContext>
-  )
+                        {(provided) => (
+                          <div
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                            className={classes.draggableDiv}
+                          >
+                            <Image
+                              src={`/images/${item.imageName}`}
+                              height={50}
+                              width={100}
+                              alt=""
+                              className={classes.tierImage}
+                            />
+                          </div>
+                        )}
+                      </Draggable>
+                    ))}
+                    {provided.placeholder}
+                  </div>
+                </div>
+              )}
+            </Droppable>
+          </div>
+        ))}
+      </DragDropContext>
+
+    </>
+  );
 }
--- lib/helper.ts
+++ lib/helper.ts
@@ -5,6 +5,21 @@ import { nanoid } from "nanoid";
 import { redirect } from "next/navigation";
 import path from "path";
 
+interface TierData{
+    S: never[];
+    A: never[];
+    B: never[];
+    C: never[];
+    D: never[];
+    Images: {
+        id: string;
+        name: string;
+        rating: string;
+        comment: string;
+        imageName: string;
+    }[];
+}
+
 export const getData = async(_prevState:{message:string},formData:FormData) =>{
     console.log('ds')
     const name = formData.get("name") as string
@@ -33,6 +48,7 @@ export const getData = async(_prevState:{message:string},formData:FormData) =>{
     const newEntry = {id,name,rating,comment,imageName}
     existingData.push(newEntry)
     await writeFile(dataUpload,JSON.stringify(existingData,null,2),"utf-8")
+    await saveNewImage()
     redirect("/")
 }
 
@@ -42,4 +58,33 @@ export const deleteData = async(id:string) =>{
     const objectValue = JSON.parse(stringedValue)
     const updatedValue = objectValue.filter((item:any) => item.id !== id)
     await writeFile("datas.json",JSON.stringify(updatedValue,null,2),"utf-8")
+}
+
+export const saveTierData = async(items:TierData)=>{
+    await writeFile("tierData.json",JSON.stringify(items,null,2),"utf-8")
+}
+
+export const saveNewImage = async()=>{  
+    const file = await readFile("datas.json")
+    const stringedValue = file.toString()
+    const objectValue = JSON.parse(stringedValue)
+
+    const secondFile = await readFile("tierData.json")
+    const secondStringedValue = secondFile.toString()
+    const secondObjectValue = JSON.parse(secondStringedValue)
+
+    objectValue.forEach((item:any) =>{
+        const isItemInTiers = 
+        secondObjectValue.S.some((tierItem:any) => tierItem.imageName === item.imageName) ||
+        secondObjectValue.A.some((tierItem:any) => tierItem.imageName === item.imageName) ||
+        secondObjectValue.B.some((tierItem:any) => tierItem.imageName === item.imageName) ||
+        secondObjectValue.C.some((tierItem:any) => tierItem.imageName === item.imageName) ||
+        secondObjectValue.D.some((tierItem:any) => tierItem.imageName === item.imageName);
+
+        if(!isItemInTiers){
+            secondObjectValue.Images.push(item)
+        }
+    })
+    await writeFile("tierData.json",JSON.stringify(secondObjectValue,null,2),"utf-8")
+
 }
